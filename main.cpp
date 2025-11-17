80357814dc73043e1c0186817cadc1f34a5e8e58
mynamasp <prasannavenkatesh918@gmail.com>
2025-11-17 15:57:52 -0500

    RISC V class created

--- main.cpp
+++ main.cpp
@@ -5,36 +5,39 @@
 #include <iomanip>
 #include <cstdint>
 
-
 using namespace std;
 
-class IF_ID {
+class IF_ID
+{
 public:
-    uint32_t IR;     
-    uint32_t NPC;     
-    bool valid;    
-    
+    uint32_t IR;
+    uint32_t NPC;
+    bool valid;
+
     IF_ID() : IR(0), NPC(0), valid(false) {}
-    
-    void clear() {
+
+    void clear()
+    {
         IR = 0;
         NPC = 0;
         valid = false;
     }
 };
 
-class ID_EX {
+class ID_EX
+{
 public:
-    uint32_t IR;      
-    uint32_t NPC;     
-    uint32_t A;       
-    uint32_t B;       
-    int32_t Imm;      
+    uint32_t IR;
+    uint32_t NPC;
+    uint32_t A;
+    uint32_t B;
+    int32_t Imm;
     bool valid;
-    
+
     ID_EX() : IR(0), NPC(0), A(0), B(0), Imm(0), valid(false) {}
-    
-    void clear() {
+
+    void clear()
+    {
         IR = 0;
         NPC = 0;
         A = 0;
@@ -44,17 +47,19 @@ public:
     }
 };
 
-class EX_MEM {
+class EX_MEM
+{
 public:
-    uint32_t IR;          
-    uint32_t B;           
-    uint32_t ALUOutput; 
-    bool cond;           
+    uint32_t IR;
+    uint32_t B;
+    uint32_t ALUOutput;
+    bool cond;
     bool valid;
-    
+
     EX_MEM() : IR(0), B(0), ALUOutput(0), cond(false), valid(false) {}
-    
-    void clear() {
+
+    void clear()
+    {
         IR = 0;
         B = 0;
         ALUOutput = 0;
@@ -63,16 +68,18 @@ public:
     }
 };
 
-class MEM_WB {
+class MEM_WB
+{
 public:
-    uint32_t IR;         
-    uint32_t ALUOutput; 
+    uint32_t IR;
+    uint32_t ALUOutput;
     uint32_t LMD;
     bool valid;
-    
+
     MEM_WB() : IR(0), ALUOutput(0), LMD(0), valid(false) {}
-    
-    void clear() {
+
+    void clear()
+    {
         IR = 0;
         ALUOutput = 0;
         LMD = 0;
@@ -80,14 +87,25 @@ public:
     }
 };
 
-class RISCVSimulator {
+class RISCV
+{
+private:
     uint32_t registers[32];
     uint32_t PC;
-    uint32_t instruction_memory[512]; 
+
+    uint32_t instruction_memory[512];
     uint32_t data_memory[512];
-    
+
     IF_ID if_id;
     ID_EX id_ex;
     EX_MEM ex_mem;
     MEM_WB mem_wb;
+
+    int totalCycles;
+    int stageUsefulCycles[5];
+    bool stall_IF;
+    bool stall_ID;
+    bool flush_IF_ID;
+
+    int instructionsCompleted;
 };
